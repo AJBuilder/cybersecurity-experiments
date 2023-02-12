cc3aa3bc54771057b890c555ec5cce411ef1065f
Adam Robertson <adam.aj.robertson@gmail.com>
2023-02-11 23:01:56 -0500

    Finished mod 3. No report

--- Module3/Task4/encryptScript.sh
+++ Module3/Task4/encryptScript.sh
@@ -11,22 +11,41 @@ done
 finish1=`date '+%s'`
 used1=`echo "$finish1-$start1" | bc`
 echo "Total time: $used1 sec"
-single1=`echo "$used1 / 10000" | bc`
-echo "That's and average of $single1 sec to encrypt once"
+single1=`echo "$used1 / 10" | bc -l`
+echo "That's an average of $single1 msec to encrypt once"
 
-openssl pkeyutl -decrypt -inkey private.key -in m_enc.txt -out m_dec.txt
+echo ""
 
 echo "####################Start####################"
 echo "Do RSA dec 10,000 times"
 echo "command line:"
-echo "openssl rsautl -encrypt -pubin -inkey public.pem -in m.txt -out m_enc.txt"
-start1=`date '+%s'`    
+echo "openssl pkeyutl -decrypt -passin pass:password -pubin -inkey public.key -in m_enc.txt -out m_dec.txt"
+start2=`date '+%s'`    
 for i in $(seq 0 10000)
 do
 openssl pkeyutl -decrypt -passin pass:password -pubin -inkey public.key -in m_enc.txt -out m_dec.txt 1> /dev/null 2>&1
 done
-finish1=`date '+%s'`
-used1=`echo "$finish1-$start1" | bc`
-echo "Total time: $used1 sec"
-single1=`echo "$used1 / 10000" | bc`
-echo "That's and average of $single1 sec to encrypt once"
+finish2=`date '+%s'`
+used2=`echo "$finish2-$start2" | bc`
+echo "Total time: $used2 sec"
+single2=`echo "$used2 / 10" | bc -l`
+echo "That's an average of $single2 msec to decrypt once"
+
+
+echo ""
+
+echo "####################Start####################"
+echo "Do AES-128-ECB encryption 10,000 times"
+echo "command line:"
+echo "openssl enc -aes-128-ecb -K 000102030405060708090A0B0C0D0E0F -in m.txt -out m_aes_enc.txt"
+start3=`date '+%s'`    
+for i in $(seq 0 10000)
+do
+openssl enc -aes-128-ecb -K 000102030405060708090A0B0C0D0E0F -in m.txt -out m_aes_enc.txt
+done
+finish3=`date '+%s'`
+used3=`echo "$finish3-$start3" | bc`
+echo "Total time: $used3 sec"
+single3=`echo "$used3 / 10" | bc -l`
+echo "That's an average of $single3 msec to encrypt once"
+
